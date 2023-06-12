d11a0b30dcb03170fa4906b67f1a8a982fb4bc09
Ruben van der Knaap <rubenvanderknaap@live.nl>
2023-06-12 10:35:48 +0200

    remove deleted branch from pip install

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
     "jwcrypto",
     "setuptools",
     "asn1crypto",
-    "pUzi @ git+ssh://git@github.com/minvws/pUzi-python@hotfix-setuptools"
+    "pUzi @ git+ssh://git@github.com/minvws/pUzi-python"
 ]
 
 setup(

157ef715702ae6320979cd9c5b26c49fc037a424
Gerbrand Bosch <gerbrandb@gmail.com>
2023-07-06 09:46:54 +0200

    removed onload, listen for load event in javascript

--- static/js/init.js
+++ static/js/init.js
@@ -1,48 +1,50 @@
-function init_page(){
-    let data = document.getElementById("info").dataset
-    let handleState = function(state){
-        if(state === "Error"){
-            window.location.assign(data.redirect_url + '?state='+ data.state +'&error=' + irmaPopup.stateMachine._state);
+(function(){
+    addEventListener("load", (event) => {
+        let data = document.getElementById("info").dataset
+        let handleState = function(state){
+            if(state === "Error"){
+                window.location.assign(data.redirect_url + '?state='+ data.state +'&error=' + irmaPopup.stateMachine._state);
+            }
         }
-    }
-    let options = {
-        // Developer options
-        debugging: false,
+        let options = {
+            // Developer options
+            debugging: false,
 
-        // Front-end options
-        language:  'en',
-        translations: {
-            header:  data.title + ' - login',
-            loading: 'Just one second please!'
-        },
-
-        // Back-end options
-        session: {
-            start: {
-                url: o => `${o.url}` + '/session/' + data.exchange_token + '/irma',
+            // Front-end options
+            language:  'en',
+            translations: {
+                header:  data.title + ' - login',
+                loading: 'Just one second please!'
             },
-            result: false,
-            mapping: {
-                sessionPtr: r => r,
+
+            // Back-end options
+            session: {
+                start: {
+                    url: o => `${o.url}` + '/session/' + data.exchange_token + '/irma',
+                },
+                result: false,
+                mapping: {
+                    sessionPtr: r => r,
+                },
             },
-        },
-    };
+        };
 
-    const irmaPopup = irma.newPopup({
-        ...options,
-        element: '#irma-web-form'
-    });
-
-    handleState(irmaPopup.stateMachine._state)
-    setInterval(function () {
-        handleState(irmaPopup.stateMachine._state)
-    }, 1000);
-
-    irmaPopup.start()
-        .then(() => {
-            window.location.assign(data.redirect_url + '?state= ' + data.state);
-        })
-        .catch((err) => {
-            window.location.assign(data.redirect_url + '?state= ' + data.state + '&error=' + irmaPopup.stateMachine._state);
+        const irmaPopup = irma.newPopup({
+            ...options,
+            element: '#irma-web-form'
         });
-}
+
+        handleState(irmaPopup.stateMachine._state)
+        setInterval(function () {
+            handleState(irmaPopup.stateMachine._state)
+        }, 1000);
+
+        irmaPopup.start()
+            .then(() => {
+                window.location.assign(data.redirect_url + '?state= ' + data.state);
+            })
+            .catch((err) => {
+                window.location.assign(data.redirect_url + '?state= ' + data.state + '&error=' + irmaPopup.stateMachine._state);
+            });
+    });
+})();

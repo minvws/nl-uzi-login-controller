38f4a5db04e6163943935a5629002074ab27bb61
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2024-03-26 14:30:49 +0100

    fix: added status 404 to session not found exception

--- app/exceptions/app_exception_handler.py
+++ app/exceptions/app_exception_handler.py
@@ -1,5 +1,6 @@
 from starlette.requests import Request
 from starlette.responses import RedirectResponse, JSONResponse, Response
+from fastapi import HTTPException
 
 from app.exceptions.app_exceptions import RedirectBaseException
 
@@ -9,3 +10,7 @@ def general_exception_handler(_request: Request, exception: Exception) -> Respon
         return RedirectResponse(url=exception.redirect_url, status_code=302)
 
     return JSONResponse("Internal Server Error", status_code=500)
+
+
+def http_exception_handler(_request: Request, exception: HTTPException) -> Response:
+    return Response(status_code=exception.status_code, content=exception.detail)

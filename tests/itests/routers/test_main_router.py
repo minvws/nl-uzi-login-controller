8d389ea10332bfaa8d9c2bbb7b0592f281c883af
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-05-28 09:34:43 +0200

    Add simple main router test

---
+++ tests/itests/routers/test_main_router.py
@@ -0,0 +1,17 @@
+from starlette.testclient import TestClient
+from starlette import status
+
+from app.main import run_app
+
+def test_root_endpoint_returns_welcome_message():
+    app = run_app()
+    with TestClient(app) as client:
+        response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.text == "Welcome to the login controller!"
+
+def test_root_endpoint_handles_invalid_methods():
+    app = run_app()
+    with TestClient(app) as client:
+        response = client.post("/")
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED

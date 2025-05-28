002fcdaee7002efe8e153c8e59e50d982f4ec4bd
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-05-28 13:46:54 +0200

    Added few tests for session service

--- tests/itests/routers/test_main_router.py
+++ tests/itests/routers/test_main_router.py
@@ -3,12 +3,14 @@ from starlette import status
 
 from app.main import run_app
 
+
 def test_root_endpoint_returns_welcome_message():
     app = run_app()
     with TestClient(app) as client:
         response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.text == "Welcome to the login controller!"
+
 
 def test_root_endpoint_handles_invalid_methods():
     app = run_app()
